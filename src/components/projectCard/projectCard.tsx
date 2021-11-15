4e40292d509d25abb77c13c451323c81f8d9c149
Nate Wolfe <nate@wxlfe.dev>
2021-11-15 10:21:25 -0500

    Improved Project Cards

--- src/components/projectCard/projectCard.tsx
+++ src/components/projectCard/projectCard.tsx
@@ -7,13 +7,15 @@ import './projectCard.sass'
 const ProjectCard = ({ project }) => (
   <Link to={'/project/' + project?.slug?.current} className='card-link'>
     <div className='card'>
-      <h3 className='card-title'>{project?.title}</h3>
-      {
-        !!project?.job?.company
-        ? (<h4 className='card-year'>{project?.year} at {project?.job?.company}</h4>)
-        : (<h4 className='card-year'>{project?.year}</h4>)
-      }
-      <GatsbyImage alt={`${project?.title} Image`} image={project?.image?.asset?.gatsbyImageData}/>
+      <div className='card-label'>
+        <h3 className='card-title'>{project?.title}</h3>
+        {
+          !!project?.job?.company
+          ? (<h4 className='card-year'>{project?.year} at {project?.job?.company}</h4>)
+          : (<h4 className='card-year'>{project?.year}</h4>)
+        }
+      </div>
+      <GatsbyImage className='card-image' alt={`${project?.title} Image`} image={project?.image?.asset?.gatsbyImageData}/>
     </div>
   </Link>
 )
