065b7f20960aee847f4fa1831fb15b15af05de86
Nate Wolfe <nate@wxlfe.dev>
2022-10-15 18:41:38 -0400

    sanity init

---
+++ sanity.ts
@@ -0,0 +1,15 @@
+import { createClient } from 'next-sanity';
+import builder from '@sanity/image-url';
+
+export const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  apiVersion: '2022-10-15',
+  useCdn: process.env.NODE_ENV === 'production',
+};
+
+export const sanityClient = createClient(config);
+
+export const urlFor = (source: any) => {
+  builder(config).image(source);
+};
