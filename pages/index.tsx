596b9f3e363b868a808c01921076193fccd1fdbc
Nate Wolfe <nate@wxlfe.dev>
2022-10-23 16:32:37 -0400

    Removed Unnecessary Code

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,42 +7,40 @@ const Index = ({ homepage }) => {
   return (
     <Layout>
       {homepage.map((section, index) => {
-        if (index % 2 === 0) {
-          return (
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-              }}
-            >
-              <Grid.Container justify='center'>
-                <Grid xs={12} md={6} css={{ padding: '1.1rem' }}>
-                  <Image
-                    src={urlFor(section?.mainImage).url()}
-                    objectFit='scale-down'
-                    alt='Default Image'
-                    css={{
-                      borderRadius: '50% 25%',
-                    }}
-                  />
-                </Grid>
-                <Grid xs={12} md={6} css={{ padding: '1.1rem' }}>
-                  <div
-                    style={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      justifyContent: 'center',
-                    }}
-                  >
-                    <Text h1>{section.title}</Text>
-                    <Text h2>{section.subtitle}</Text>
-                  </div>
-                </Grid>
-              </Grid.Container>
-            </div>
-          );
-        }
+        return (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+            }}
+          >
+            <Grid.Container justify='center'>
+              <Grid xs={12} md={6} css={{ padding: '1.1rem' }}>
+                <Image
+                  src={urlFor(section?.mainImage).url()}
+                  objectFit='scale-down'
+                  alt='Default Image'
+                  css={{
+                    borderRadius: '50% 25%',
+                  }}
+                />
+              </Grid>
+              <Grid xs={12} md={6} css={{ padding: '1.1rem' }}>
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <Text h1>{section.title}</Text>
+                  <Text h2>{section.subtitle}</Text>
+                </div>
+              </Grid>
+            </Grid.Container>
+          </div>
+        );
       })}
     </Layout>
   );
