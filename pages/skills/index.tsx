8b043013bef40bdd35b431661d4a5b03dcda5f22
Nate Wolfe <nate@wxlfe.dev>
2022-10-30 20:00:18 -0400

    Sorted Skills by Experience

--- pages/skills/index.tsx
+++ pages/skills/index.tsx
@@ -3,41 +3,47 @@ import { Layout } from 'components';
 import { urlFor } from 'sanity';
 import { fetchSkills } from 'utils';
 
+function sortByExperience(a, b) {
+  if (a.experience > b.experience) {
+    return -1;
+  }
+  if (b.experience > a.experience) {
+    return 1;
+  }
+  return 0;
+}
+
 const skills = ({ skills }) => {
   return (
     <Layout>
       <Text h1 css={{ textAlign: 'center' }}>
         Skills
       </Text>
-      <Grid.Container gap={1} justify='center'>
-        {skills.map((skill, index) => {
-          return (
-            <Grid xs={6} sm={4} justify='center' key={index}>
-              <Link href={`/skills/${skill.slug.current}`}>
-                <Button
-                  color='primary'
-                  auto
-                  rounded
-                  ghost
-                  icon={
-                    <Image
-                      src={urlFor(skill.skillIcon).url()}
-                      css={{
-                        objectFit: 'scale-down',
-                        maxHeight: '2rem',
-                        maxWidth: '2rem',
-                      }}
-                    />
-                  }
-                  href={`/skills/${skill.slug.current}`}
-                >
-                  <Text b>{skill.title}</Text>
-                </Button>
-              </Link>
-            </Grid>
-          );
-        })}
-      </Grid.Container>
+      {skills.sort(sortByExperience).map((skill, index) => {
+        return (
+          <Link href={`/skills/${skill.slug.current}`}>
+            <Button
+              color='primary'
+              auto
+              rounded
+              ghost
+              icon={
+                <Image
+                  src={urlFor(skill.skillIcon).url()}
+                  css={{
+                    objectFit: 'scale-down',
+                    maxHeight: '2rem',
+                    maxWidth: '2rem',
+                  }}
+                />
+              }
+              href={`/skills/${skill.slug.current}`}
+            >
+              <Text b>{skill.title}</Text>
+            </Button>
+          </Link>
+        );
+      })}
     </Layout>
   );
 };
