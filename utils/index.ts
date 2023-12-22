27301e3194b67d69ae3ca84b83f9a09d708c0219
Nate Wolfe <nate@wxlfe.dev>
2023-12-22 17:05:32 -0500

    Removed Old Site

--- utils/index.ts
+++
@@ -1,8 +0,0 @@
-export { fetchExperience } from './fetchExperience';
-export { fetchHomepage } from './fetchHomepage';
-export { fetchProjects } from './fetchProjects';
-export { fetchSkills } from './fetchSkills';
-export { fetchSkill } from './fetchSkill';
-export { fetchProject } from './fetchProject';
-export { fetchSkillPaths } from './fetchSkillPaths';
-export { fetchProjectPaths } from './fetchProjectPaths';
