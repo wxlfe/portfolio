86493d870d06f77ee99d1774b3f91751a93cbf45
Nate Wolfe <nate@wxlfe.dev>
2023-12-22 22:00:20 -0500

    Added Home Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ProjectsComponent } from './projects/projects.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ExperienceComponent } from './experience/experience.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   { path: 'projects', component: ProjectsComponent },
   { path: 'skills', component: SkillsComponent },
   { path: 'experience', component: ExperienceComponent },
+  { path: '', component: HomeComponent },
 ];
