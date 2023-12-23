8765c1a1cbbd88d7874cd4baf5135fd0c3cda959
Nate Wolfe <nate@wxlfe.dev>
2023-12-22 21:56:07 -0500

    Pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatTabsModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+  navLinks: any[];
+  activeLinkIndex = -1;
+
+  constructor(private router: Router) {
+    this.navLinks = [
+      {
+        label: 'Projects',
+        link: './projects',
+        index: 0,
+      },
+      {
+        label: 'Skills',
+        link: './skills',
+        index: 1,
+      },
+      {
+        label: 'Experience',
+        link: './experience',
+        index: 2,
+      },
+    ];
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((res) => {
+      this.activeLinkIndex = this.navLinks.indexOf(
+        this.navLinks.find((tab) => tab.link === '.' + this.router.url)
+      );
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'experience', component: ExperienceComponent },
+];
